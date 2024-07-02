98fd8810274192fc377828133384ad92db48867f
chelsy2001 <chelsyyadav22@gmail.com>
2024-07-03 00:59:16 +0530

    Homepage

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import './styles.scss'
+import { useNavigate } from 'react-router-dom'
+import {Animate} from 'react-simple-animate'
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  console.log(navigate)
+
+   const handleNavigationToContactMePage = () =>{
+    navigate("/contact");
+   }
   return (
-    <div>
-      Home
-    </div>
+    <section id='home' className='home'>
+      <div className='home__text-wrapper'>
+        <h1>
+          Hello, I'm Chelsy
+          <br/>
+          React Native Developer
+        </h1>
+      </div> 
+      <Animate
+      play
+      duration={1.5}
+      delay={1}
+      start={{
+        transform:'translateY(550px)'
+      }}
+      end={{
+        transform:'translatex(0px)'
+      }}>
+      <div className='home__contact-me'>
+        <button onClick={handleNavigationToContactMePage}>Hire Me</button>
+      </div>
+      </Animate>
+    </section>
   )
 }
 
