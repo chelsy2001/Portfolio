818164c5c4ab1e678ec732ebde13ec77c89439f4
chelsy2001 <chelsyyadav22@gmail.com>
2024-07-04 03:30:14 +0530

    skills page

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
+import PageHeaderContent from '../../components/pageHeaderContent'
+import { BsInfoCircleFill } from 'react-icons/bs'
+import { Animate, AnimateKeyframes } from 'react-simple-animate'
+import { skillData } from './utils'
+import { Line } from 'rc-progress'
+import './styles.scss' // Ensure you import the SCSS file
 
-const Skills = () => {
+const Skill = () => {
   return (
-    <div>
-      Skills
-    </div>
-  )
+    <section id='skill' className='skill'>
+      <PageHeaderContent 
+        headerText="My Skills"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+
+      <div className='skills__content-wrapper'>
+        {skillData.map((item, i) => {
+          return (
+            <div key={i} className='skills__content-wrapper__inner-content'>
+              <Animate
+                play
+                duration={1}
+                delay={0.3}
+                start={{ transform: 'translateX(-200px)' }}
+                end={{ transform: 'translateX(0px)' }}
+              >
+                <h3 className='skills__content-wrapper__inner-content__category-text'>{item.label}</h3>
+                <div>
+                  {item.data.map((skillitem, j) => (
+                    <AnimateKeyframes
+                      play
+                      duration="1"
+                      keyframes={['opacity:1', 'opacity:0']}
+                      iterationCount="1"
+                    >
+                      <div className='progressbar-wrapper' key={j}>
+                      <p>{skillitem.skillName}</p>
+                        <Line 
+                          percent={skillitem.percentage}
+                          strokeWidth="5"
+                          strokeColor="var(--yellow-theme-main-color)"
+                          trailWidth="5"
+                          strokeLinecap='square'
+                        />
+                      </div>
+                    </AnimateKeyframes>
+                  ))}
+                </div>
+              </Animate>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skill;
---
+++ src/containers/skills/utils.js
@@ -0,0 +1,61 @@
+export const skillData = [
+    {
+        label: "FRONT END",
+        data:[
+            {
+                skillName: "HTML",
+                percentage:"90",
+            },
+            {
+                skillName: "Css",
+                percentage:"60",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage:"80",
+            },
+            {
+                skillName: "TypeScript",
+                percentage:"50",
+            },
+        ],
+    },
+    {
+        label:"FrameWork",
+        data:[
+            {
+                skillName: "React Native",
+                percentage:"80",
+            },
+            {
+                skillName: "React",
+                percentage:"50",
+            },
+            {
+                skillName: "Redux",
+                percentage:"50",
+            },
+        ],
+    },
+    {
+        label:"DATABASES",
+        data:[
+            {
+                skillName: "MySQL",
+                percentage:"80",
+            },
+            {
+                skillName: "MONGODB",
+                percentage:"50",
+            },
+            {
+                skillName: "GraphQL",
+                percentage:"60",
+            },
+            {
+                skillName: "postgresql",
+                percentage:"50",
+            },
+        ]
+    }
+]
