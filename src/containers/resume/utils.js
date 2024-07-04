6e41854bb60050b695616407919657e5c29caa5b
chelsy2001 <chelsyyadav22@gmail.com>
2024-07-04 18:18:07 +0530

    Resume page

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,36 @@
+export const data = {
+    exprience:[
+        {
+            title:"React Native Developer",
+            subTitle: "Badho Technology",
+            description:"Led enhancements for Badho's mobile app and its variants,Revamped navigation interfaces, enhancing user experience,Developed dynamic features like Notification Center and Schemes screen,bottom nav , side menu bar.",
+            date:"july/2023-present"
+        },
+        {
+            title:"Front-end Developer Intern ",
+            subTitle:"Helpy Motors",
+            description:"Created a fully responsive About page for Helpy Motors, detailing its history, mission, and values,Developed a user-friendly contact form using React, HTML, and JavaScript, with validation for accurate submissions,Enhanced site responsiveness and updated the design for better readability and visual appeal",
+            date:"Dec/2022-May/2023"
+        },
+    ],
+    education:[
+        {
+            title:"Secondary",
+            subTitle:"Samarth Bal Mandir School",
+            description:"10 Grade",
+            date:"2016-2017"
+        },
+        {
+            title:"Higher Secondary",
+            subTitle:"Shri Gauri Shankar School",
+            description:"12 Grade in PCM",
+            date:"2018-2019"
+        },
+        {
+            title:"B.Tech",
+            subTitle:"Ips College Of Technology And Managment",
+            description:"Bachelor of Technology in Computer Science",
+            date:"2019-2023"
+        },
+    ],
+};
